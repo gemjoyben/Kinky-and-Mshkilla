a5f4be32dabb0441e6cf6080ab55bdba4c16f6c3
gemjoyben <gemjoyben@hotmail.com>
2021-01-16 17:49:01 +0200

    final comment just to answer the quiz

--- My first script.R
+++ My first script.R
@@ -2,3 +2,5 @@ print("This file was created within RStudio")
 print("And now it lives on GitHub")
 
 print("this is a new change so i can see the process again, see you on github!")
+
+print("and a final comment to answer the quiz")
